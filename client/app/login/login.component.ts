56025a18fb58887984143c4e1042b27ca5d4bd45
349032805@qq.com <349032805@qq.com>
2017-08-01 19:11:04 +0800

    git commit

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 import { ToastComponent } from '../shared/toast/toast.component';
 
 @Component({
@@ -11,42 +12,82 @@ import { ToastComponent } from '../shared/toast/toast.component';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-
+  // ****************************登录部分*******************
   loginForm: FormGroup;
-  email = new FormControl('', [Validators.required,
+  loginEmail = new FormControl('', [Validators.required,
                                        Validators.minLength(3),
                                        Validators.maxLength(100)]);
-  password = new FormControl('', [Validators.required,
+  loginPassword = new FormControl('', [Validators.required,
                                           Validators.minLength(6)]);
   //切换tab
   tab ='login';
+  // ****************************注册部分*******************
+  registerForm: FormGroup;
+  username = new FormControl('', [Validators.required,
+                                  Validators.minLength(2),
+                                  Validators.maxLength(30),
+                                  Validators.pattern('[a-zA-Z0-9_-\\s]*')]);
+
+  registerEmail = new FormControl('', [Validators.required,Validators.minLength(3),Validators.maxLength(100)]);
+  registerPassword = new FormControl('', [Validators.required,Validators.minLength(6)]);
+
+  role = new FormControl('', [Validators.required]);
 
   constructor(private auth: AuthService,
               private formBuilder: FormBuilder,
               private router: Router,
-              public toast: ToastComponent) { }
+              public toast: ToastComponent,
+              private userService: UserService) { }
 
   ngOnInit() {
     if (this.auth.loggedIn) {
-      this.router.navigate(['/']);
+      this.router.navigate(['/cats']);
     }
     this.loginForm = this.formBuilder.group({
-      email: this.email,
-      password: this.password
+      loginEmail: this.loginEmail,
+      loginPassword: this.loginPassword
+    });
+
+    this.registerForm = this.formBuilder.group({
+      username: this.username,
+      registerEmail: this.registerEmail,
+      registerPassword: this.registerPassword,
+      role: this.role
     });
   }
 
-  setClassEmail() {
-    return { 'has-danger': !this.email.pristine && !this.email.valid };
+  setClassLoginEmail() {
+    return { 'has-danger': !this.loginEmail.pristine && !this.loginEmail.valid };
   }
-  setClassPassword() {
-    return { 'has-danger': !this.password.pristine && !this.password.valid };
+  setClassLoginPassword() {
+    return { 'has-danger': !this.loginPassword.pristine && !this.loginPassword.valid };
+  }
+
+  setClassRegisterEmail() {
+    return { 'has-danger': !this.registerEmail.pristine && !this.registerEmail.valid };
+  }
+  setClassRegisterPassword() {
+    return { 'has-danger': !this.registerPassword.pristine && !this.registerPassword.valid };
+  }
+
+  setClassUsername() {
+    return { 'has-danger': !this.username.pristine && !this.username.valid };
   }
 
   login() {
     this.auth.login(this.loginForm.value).subscribe(
-      res => this.router.navigate(['/']),
-      error => this.toast.setMessage('invalid email or password!', 'danger')
+      res => this.router.navigate(['/cats']),
+      error => this.toast.setMessage('无效的邮箱或密码!', 'danger')
+    );
+  }
+
+   register() {
+    this.userService.register(this.registerForm.value).subscribe(
+      res => {
+        this.toast.setMessage('注册成功!', 'success');
+        this.router.navigate(['/']);
+      },
+      error => this.toast.setMessage('邮箱已存在', 'danger')
     );
   }
 
